fbf1a2d458616c6fbe17f2ec7593bb2c3358e074
OoC.moe <196446122+oocmoe@users.noreply.github.com>
2025-06-14 15:57:10 +0800

    feat: nav card icon support

--- components/card-navItem.tsx
+++ components/card-navItem.tsx
@@ -9,14 +9,16 @@ type CardNavItemProps = {
 	label: string;
 	href: Href;
 	status?: "active" | "offline";
+	icon?:React.ComponentType<any>;
 };
 
-export const CardNavItem = ({ label, href, status }: CardNavItemProps) => {
+export const CardNavItem = ({ label, href, status,icon }: CardNavItemProps) => {
 	return (
 		<Pressable className="active:opacity-80" onPress={() => router.push(href)}>
 			<View className="flex flex-row justify-between items-center p-3 border border-slate-200 dark:border-slate-800 rounded-md">
 				<View className="flex-1 min-w-0 flex-row items-center gap-x-1">
 					{status && <StatusBadge status={status} />}
+					{icon && <Icon as={icon} />}
 					<Text className="truncate flex-shrink font-bold" numberOfLines={1} ellipsizeMode="tail">
 						{label}
 					</Text>
